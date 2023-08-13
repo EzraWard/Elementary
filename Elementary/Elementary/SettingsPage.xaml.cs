9370da2a8a77829f2057a2e159211122993828d8
EzraWard <52414057+EzraWard@users.noreply.github.com>
2023-08-12 22:04:46 -0400

    Settings page version no longer hard coded
    Version is pulled from the app at runtime.

--- Elementary/Elementary/SettingsPage.xaml.cs
+++ Elementary/Elementary/SettingsPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.ApplicationModel;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -24,7 +12,19 @@ namespace Elementary
     {
         public SettingsPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            VersionTextBlock.Text = GetApplicationVersion();
+        }
+
+        public static string GetApplicationVersion()
+        {
+            Package package = Package.Current;
+            PackageId packageId = package.Id;
+            PackageVersion version = packageId.Version;
+
+            //return string.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
+            return string.Format("{0}.{1}.{2}", version.Major, version.Minor, version.Build);
         }
     }
 }
