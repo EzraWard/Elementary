4d6b30dfbc23cb22c6a23514caa30913df50efde
EzraWard <52414057+EzraWard@users.noreply.github.com>
2024-06-24 23:56:59 -0400

    Move pages to Pages folder

--- Elementary/Elementary/App.xaml.cs
+++ Elementary/Elementary/App.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.ApplicationModel.Core;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
