e6c34fcf6c67a7705c95a14d6054449c759ebcb7
EzraWard <52414057+EzraWard@users.noreply.github.com>
2025-01-06 22:02:49 -0500

    More work

---
+++ src/Elementary.UWP/Dictionaries/EBookToEPubLocationNET.cs
@@ -0,0 +1,83 @@
+﻿using Elementary.Core.Enums;
+using Elementary.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elementary.Dictionaries
+{
+    public static class EBookToLocation
+    {
+        public static readonly Dictionary<EBook, int> EBookToEPubLocationNET = new Dictionary<EBook, int>
+        {
+            { EBook.Genesis, 2 },
+            { EBook.Exodus, 53 },
+            { EBook.Leviticus, 94 },
+            { EBook.Numbers, 122 },
+            { EBook.Deuteronomy, 159 },
+            { EBook.Joshua, 194 },
+            { EBook.Judges, 219 },
+            { EBook.Ruth, 241 },
+            { EBook.FirstSamuel, 246 },
+            { EBook.SecondSamuel, 278 },
+            { EBook.FirstKings, 303 },
+            { EBook.SecondKings, 326 },
+            { EBook.FirstChronicles, 352 },
+            { EBook.SecondChronicles, 382 },
+            { EBook.Ezra, 419 },
+            { EBook.Nehemiah, 430 },
+            { EBook.Esther, 444 },
+            { EBook.Job, 455 },
+            { EBook.Psalms, 498 },
+            { EBook.Proverbs, 649 },
+            { EBook.Ecclesiastes, 681 },
+            { EBook.SongOfSolomon, 694 },
+            { EBook.Isaiah, 703 },
+            { EBook.Jeremiah, 770 },
+            { EBook.Lamentations, 823 },
+            { EBook.Ezekiel, 829 },
+            { EBook.Daniel, 878 },
+            { EBook.Hosea, 891 },
+            { EBook.Joel, 906 },
+            { EBook.Amos, 910 },
+            { EBook.Obadiah, 920 },
+            { EBook.Jonah, 922 },
+            { EBook.Micah, 927 },
+            { EBook.Nahum, 935 },
+            { EBook.Habakkuk, 939 },
+            { EBook.Zephaniah, 943 },
+            { EBook.Haggai, 947 },
+            { EBook.Zechariah, 950 },
+            { EBook.Malachi, 965 },
+            { EBook.Matthew, 970 },
+            { EBook.Mark, 999 },
+            { EBook.Luke, 1016 },
+            { EBook.John, 1041 },
+            { EBook.Acts, 1063 },
+            { EBook.Romans, 1092 },
+            { EBook.FirstCorinthians, 1109 },
+            { EBook.SecondCorinthians, 1126 },
+            { EBook.Galatians, 1140 },
+            { EBook.Ephesians, 1147 },
+            { EBook.Philippians, 1154 },
+            { EBook.Colossians, 1159 },
+            { EBook.FirstThessalonians, 1164 },
+            { EBook.SecondThessalonians, 1170 },
+            { EBook.FirstTimothy, 1174 },
+            { EBook.SecondTimothy, 1181 },
+            { EBook.Titus, 1186 },
+            { EBook.Philemon, 1190 },
+            { EBook.Hebrews, 1192 },
+            { EBook.James, 1206 },
+            { EBook.FirstPeter, 1212 },
+            { EBook.SecondPeter, 1218 },
+            { EBook.FirstJohn, 1222 },
+            { EBook.SecondJohn, 1228 },
+            { EBook.ThirdJohn, 1230 },
+            { EBook.Jude, 1232 },
+            { EBook.Revelation, 1234 }
+        };
+    }
+}
--- src/Elementary.UWP/ViewModels/BiblePageViewModel.cs
+++ src/Elementary.UWP/ViewModels/BiblePageViewModel.cs
@@ -124,7 +124,7 @@ namespace Elementary.ViewModels
             }
             foreach (var book in Bible.Books)
             {
-                book.ReadingOrderIndex = GetStartingPointofBook(book);
+                book.ReadingOrderIndex = 1;
             }
             for(int i = 0; i < Bible.Books.Count; i++) 
             {
@@ -145,8 +145,8 @@ namespace Elementary.ViewModels
                 }
             }
 
-            Book = Bible.Books.SingleOrDefault(i => i.Title == (string) AppSettings.Book);
-            Chapter = Book.Chapters[int.Parse((string) currentChapter) - 1];
+            // = Bible.Books.SingleOrDefault(i => i.Title == (string) AppSettings.Book);
+            //Chapter = Book.Chapters[int.Parse((string) currentChapter) - 1];
 
             //First chapter in Genesis
 
@@ -169,147 +169,6 @@ namespace Elementary.ViewModels
             //    brTag.Remove();
             var test = htmlDoc.DocumentNode.SelectSingleNode("//body").InnerHtml;
             CurrentChapterContent = test;
-        }
-
-        private int GetStartingPointofBook(Book book)
-        {
-            switch (book.Title)
-            {
-                case "Genesis":
-                    return 2;                    
-                case "Exodus":
-                    return 53;
-                case "Leviticus":
-                    return 94;
-                case "Numbers":
-                    return 122;
-                case "Deuteronomy":
-                    return 159;
-                case "Joshua":
-                    return 194;
-                case "Judges":
-                    return 219;
-                case "Ruth":
-                    return 241;
-                case "1 Samuel":
-                    return 246;
-                case "2 Samuel":
-                    return 278;
-                case "1 Kings":
-                    return 303;
-                case "2 Kings":
-                    return 326;
-                case "1 Chronicles":
-                    return 340 + 12;
-                case "2 Chronicles":
-                    return 369 + 13;
-                case "Ezra":
-                    return 405 + 14;
-                case "Nehemiah":
-                    return 415 + 15;
-                case "Esther":
-                    return 428 + 16;
-                case "Job":
-                    return 438 + 17;
-                case "Psalms":
-                    return 480 + 18;
-                case "Proverbs":
-                    return 630 + 19;
-                case "Ecclesiastes":
-                    return 661 + 20;
-                case "Song of Solomon":
-                    return 673 + 21;
-                case "Isaiah":
-                    return 681 + 22;
-                case "Jeremiah":
-                    return 747 + 23;
-                case "Lamentations":
-                    return 799 + 24;
-                case "Ezekiel":
-                    return 804 + 25;
-                case "Daniel":
-                    return 852 + 26;
-                case "Hosea":
-                    return 864 + 27;
-                case "Joel":
-                    return 878 + 28;
-                case "Amos":
-                    return 881 + 29;
-                case "Obadiah":
-                    return 890 + 30;
-                case "Jonah":
-                    return 891 + 31;
-                case "Micah":
-                    return 895 + 32;
-                case "Nahum":
-                    return 902 + 33;
-                case "Habakkuk":
-                    return 905 + 34;
-                case "Zephaniah":
-                    return 908 + 35;
-                case "Haggai":
-                    return 911 + 36;
-                case "Zechariah":
-                    return 913 + 37;
-                case "Malachi":
-                    return 927 + 38;
-                case "Matthew":
-                    return 931 + 39;
-                case "Mark":
-                    return 959 + 40;
-                case "Luke":
-                    return 975 + 41;
-                case "John":
-                    return 999 + 42;
-                case "Acts":
-                    return 1020 + 43;
-                case "Romans":
-                    return 1048 + 44;
-                case "1 Corinthians":
-                    return 1064 + 45;
-                case "2 Corinthians":
-                    return 1080 + 46;
-                case "Galatians":
-                    return 1093 + 47;
-                case "Ephesians":
-                    return 1099 + 48;
-                case "Philippians":
-                    return 1105 + 49;
-                case "Colossians":
-                    return 1109 + 50;
-                case "1 Thessalonians":
-                    return 1113 + 51;
-                case "2 Thessalonians":
-                    return 1118 + 52;
-                case "1 Timothy":
-                    return 1121 + 53;
-                case "2 Timothy":
-                    return 1127 + 54;
-                case "Titus":
-                    return 1131 + 55;
-                case "Philemon":
-                    return 1134 + 56;
-                case "Hebrews":
-                    return 1135 + 57;
-                case "James":
-                    return 1148 + 58;
-                case "1 Peter":
-                    return 1153 + 59;
-                case "2 Peter":
-                    return 1158 + 60;
-                case "1 John":
-                    return 1161 + 61;
-                case "2 John":
-                    return 1166 + 62;
-                case "3 John":
-                    return 1167 + 63;
-                case "Jude":
-                    return 1167 + 65;
-                case "Revelation":
-                    return 1169 + 65;
-                default:
-                    return 0;
-            }
         }
     }
 }
