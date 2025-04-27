610cbd1d5e1cc9ca5cf30b456172653f85ffc71c
EzraWard <52414057+EzraWard@users.noreply.github.com>
2025-04-26 23:40:44 -0400

    implementation of NET in bible service

--- src/Elementary.Core/Services/BibleService.cs
+++ src/Elementary.Core/Services/BibleService.cs
@@ -2,13 +2,28 @@
 using Elementary.Core.Interfaces;
 using Elementary.Core.Models;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using VersOne.Epub;
 
 namespace Elementary.Core.Services
 {
     public class BibleService : IBibleService
     {
+        private readonly ISettingsService _settingsService;
+        private readonly IFileService _fileService;
+        private readonly IFilePathProvider _filePathProvider;
+        private readonly ISettings _settings;
+
+        public BibleService(ISettingsService settingsService, IFileService fileService, IFilePathProvider filePathProvider)
+        {
+            _settingsService = settingsService ?? throw new ArgumentNullException(nameof(settingsService));
+            _settings = _settingsService.GetSettings();
+
+            _fileService = fileService;
+
+            _filePathProvider = filePathProvider;
+        }
+
         public Bible GetBible(ETranslation translation)
         {
             switch (translation)
@@ -36,60 +51,43 @@ namespace Elementary.Core.Services
 
         private Bible GetBibleNET()
         {
-            return new Bible();
+            var bible = new Bible();
 
-        //    var biblePath = BiblePathDictionary.BibleDictionary[AppSettings.Translation.ToString()];
-        //    var bibleFilePath = StorageFile.GetFileFromApplicationUriAsync(new Uri(biblePath)).AsTask().Result.Path;
+            var bibleFilePath = _filePathProvider.GetPathForTranslation(ETranslation.NET);
+            var epubBible = EpubReader.ReadBook(bibleFilePath);
 
-        //    _currentBible = EpubReader.ReadBook(bibleFilePath);
+            //Enumerate Books
+            foreach (var book in epubBible.Navigation)
+            {
+                bible.Books.Add(new Book
+                {
+                    Title = book.Title
+                });
+            }
+            foreach (var book in bible.Books)
+            {
+                book.ReadingOrderIndex = 1;
+            }
+            for (int i = 0; i < bible.Books.Count; i++)
+            {
+                int numberOfChapters;
+                if (bible.Books[i].Title != "Revelation")
+                {
+                    numberOfChapters = bible.Books[i + 1].ReadingOrderIndex - bible.Books[i].ReadingOrderIndex;
+                }
+                else
+                {
+                    numberOfChapters = 22;
+                }
 
-        //    //Enumerate Books
-        //    _currentBibleBooks = new List<string>();
-        //    Bible = new Bible();
-        //    foreach (var book in _currentBible.Navigation)
-        //    {
-        //        Bible.Books.Add(new Book
-        //        {
-        //            Title = book.Title
-        //        });
-        //    }
-        //    foreach (var book in Bible.Books)
-        //    {
-        //        book.ReadingOrderIndex = 1;
-        //    }
-        //    for (int i = 0; i < Bible.Books.Count; i++)
-        //    {
-        //        int numberOfChapters;
-        //        if (Bible.Books[i].Title != "Revelation")
-        //        {
-        //            numberOfChapters = Bible.Books[i + 1].ReadingOrderIndex - Bible.Books[i].ReadingOrderIndex;
-        //        }
-        //        else
-        //        {
-        //            numberOfChapters = 22;
-        //        }
+                bible.Books[i].Chapters = new ObservableCollection<Chapter>();
+                for (int j = 1; j < numberOfChapters; j++)
+                {
+                    bible.Books[i].Chapters.Add(new Chapter { Index = j, ReadingOrderIndex = bible.Books[i].ReadingOrderIndex + j });
+                }
+            }
 
-        //        Bible.Books[i].Chapters = new ObservableCollection<Chapter>();
-        //        for (int j = 1; j < numberOfChapters; j++)
-        //        {
-        //            Bible.Books[i].Chapters.Add(new Chapter { Index = j, ReadingOrderIndex = Bible.Books[i].ReadingOrderIndex + j });
-        //        }
-        //    }
-
-        //    // = Bible.Books.SingleOrDefault(i => i.Title == (string) AppSettings.Book);
-        //    //Chapter = Book.Chapters[int.Parse((string) currentChapter) - 1];
-
-        //    //First chapter in Genesis
-
-        //    //var content = _currentBible.ReadingOrder[Chapter.ReadingOrderIndex].Content;
-        //    //var match = Regex.Match(content, "(.*<\\s* body[^>]*>)| (<\\s */\\s* body\\s *\\>.+)");
-        //    var htmlDoc = new HtmlDocument();
-        //    htmlDoc.OptionWriteEmptyNodes = true;
-        //    htmlDoc.LoadHtml(_currentBible.ReadingOrder[Chapter.ReadingOrderIndex].Content);
-        //    //foreach (var brTag in htmlDoc.DocumentNode.SelectNodes("//br"))
-        //    //    brTag.Remove();
-        //    CurrentChapterContent = htmlDoc.DocumentNode.SelectSingleNode("//body").InnerHtml;
-        //}
+            return bible;
         }
     }
 }
--- src/Elementary.UWP/Pages/BiblePage.xaml.cs
+++ src/Elementary.UWP/Pages/BiblePage.xaml.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.Storage;
-using Windows.UI.Xaml.Controls.Primitives;
 
 namespace Elementary
 {
