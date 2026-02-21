ed24f90d7d509eb5f749bbf7751e27f5eec71148
EzraWard <52414057+EzraWard@users.noreply.github.com>
2026-02-20 21:43:39 -0500

    ePUB removal

--- src/Elementary.UWP/Pages/MainPage.xaml.cs
+++ src/Elementary.UWP/Pages/MainPage.xaml.cs
@@ -11,7 +11,6 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Media.Imaging;
-using Microsoft.Extensions.DependencyInjection;
 using Elementary.Core.Interfaces;
 using Elementary.Core.Models;
 using System.Collections.Generic;
