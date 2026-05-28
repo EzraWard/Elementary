60566d1f5eba4899fcb58719086c619c4a87eb2c
EzraWard <52414057+EzraWard@users.noreply.github.com>
2026-05-27 21:59:39 -0400

    Fix missing class

---
+++ src/Elementary.VerseOfTheDay/ApiKeys.cs
@@ -0,0 +1,7 @@
+namespace Elementary.VerseOfTheDay
+{
+    internal static class ApiKeys
+    {
+        public const string UnsplashAccessKey = "upnkkQsybr4SOwbbZCZ3RXUnWTdWvc3TAKs0cUj3b7w";
+    }
+}
